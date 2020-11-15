dca17c899659e30f5252b26f65173b2fdf75f16d
Alex <threebowpr@gmail.com>
2020-11-15 00:17:52 -0800

    Add breadcrumb components

---
+++ components/Breadcrumb.vue
@@ -0,0 +1,26 @@
+<template lang="pug">
+	li(:class="{'is-active': active}")
+		a(:href="href")
+			template(v-if="icon")
+				icon(:icon="icon")
+				span
+					slot
+
+			template(v-else)
+				slot
+</template>
+
+<script>
+	import Icon from "./icon";
+
+	export default {
+		props: {
+			icon: String,
+			href: String,
+			active: Boolean
+		},
+		components: {
+			Icon
+		}
+	};
+</script>
---
+++ components/Breadcrumbs.vue
@@ -0,0 +1,5 @@
+<template lang="pug">
+	nav.breadcrumb
+		ul
+			slot
+</template>
