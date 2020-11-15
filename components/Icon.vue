ad99fa2361492519b2eec920b346f79e633713b0
Alex <threebowpr@gmail.com>
2020-11-15 01:15:42 -0800

    Icon component

---
+++ components/Icon.vue
@@ -0,0 +1,16 @@
+<template lang="pug">
+	span.icon
+		i(:class="[icon, fixedWidth ? 'fa-fw' : '']")
+</template>
+
+<script>
+	export default {
+		props: {
+			icon: String,
+			fixedWidth: {
+				type: Boolean,
+				default: false
+			}
+		}
+	};
+</script>
