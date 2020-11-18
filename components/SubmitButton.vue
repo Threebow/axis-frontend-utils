06676d5fe805ae00f64591973e54100d1d1b35d9
Alex <threebowpr@gmail.com>
2020-11-17 22:46:52 -0800

    Add SubmitButton component

---
+++ components/SubmitButton.vue
@@ -0,0 +1,49 @@
+<template lang="pug">
+	.submit-button-wrapper(:class="{'is-right': (isRight && !fullwidth), 'is-fullwidth': fullwidth}")
+		button.button(:class="{loading, [buttonClass]: true}", type="submit", :disabled="loading || disabled")
+			slot
+				icon(icon="fas fa-save")
+				span Save
+</template>
+
+<script>
+	export default {
+		props: {
+			isRight: {
+				type: Boolean,
+				default: true
+			},
+			fullwidth: {
+				type: Boolean,
+				default: false
+			},
+			buttonClass: {
+				type: String,
+				default: "is-primary"
+			},
+			loading: {
+				type: Boolean,
+				default: false
+			},
+			disabled: {
+				type: Boolean,
+				default: false
+			}
+		}
+	};
+</script>
+
+<style lang="scss" scoped>
+	.submit-button-wrapper {
+		display: flex;
+
+		&.is-right {
+			justify-content: flex-end;
+		}
+
+		&.is-fullwidth button {
+			flex: 1;
+			justify-content: center;
+		}
+	}
+</style>
