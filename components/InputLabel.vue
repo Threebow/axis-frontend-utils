b6165964c607ca55fde528d51d945ca9e9125bfa
Alex <threebowpr@gmail.com>
2020-11-17 22:34:58 -0800

    Add InputLabel

---
+++ components/InputLabel.vue
@@ -0,0 +1,21 @@
+<template lang="pug">
+	label.label.is-unselectable(v-if="label")
+		span.is-heading {{ label }}
+		icon.has-text-danger(v-if="required", icon="fas fa-asterisk fa-fw fa-xs")
+</template>
+
+<script>
+	export default {
+		props: {
+			label: String,
+			required: Boolean
+		}
+	};
+</script>
+
+<style lang="scss" scoped>
+	label {
+		display: flex;
+		align-items: center;
+	}
+</style>
