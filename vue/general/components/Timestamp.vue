87f7e8596ac1447c4bdd75c1504252d25890aa70
Alex <threebowpr@gmail.com>
2020-11-23 15:21:39 -0800

    Timestamp component wrapping around formatTime

---
+++ vue/general/components/Timestamp.vue
@@ -0,0 +1,34 @@
+<template lang="pug">
+	span(v-if="formatted") {{ formatted }}
+</template>
+
+<script>
+	export default {
+		props: {
+			date: [Date, String],
+			compact: {
+				type: Boolean,
+				default: true
+			},
+			dateOnly: {
+				type: Boolean,
+				default: false
+			},
+			timeOnly: {
+				type: Boolean,
+				default: false
+			}
+		},
+		computed: {
+			formatted() {
+				if(!this.date) return "~";
+
+				return this.formatDate(this.date, {
+					compact: this.compact,
+					dateOnly: this.dateOnly,
+					timeOnly: this.timeOnly
+				});
+			}
+		}
+	};
+</script>
