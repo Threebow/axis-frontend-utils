c92c8454225162befc6675ebcafd997de9bcfd3e
Alex <threebowpr@gmail.com>
2020-11-23 14:10:32 -0800

    Button group component

---
+++ vue/general/components/Buttons/Buttons.vue
@@ -0,0 +1,23 @@
+<template lang="pug">
+	.button-group(:class="{'is-stack': stack, 'is-centered': centered, 'is-opposite': opposite}")
+		slot
+</template>
+
+<script>
+	export default {
+		props: {
+			stack: {
+				type: Boolean,
+				default: false
+			},
+			centered: {
+				type: Boolean,
+				default: false
+			},
+			opposite: {
+				type: Boolean,
+				default: false
+			}
+		}
+	};
+</script>
