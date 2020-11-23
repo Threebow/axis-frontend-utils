9df629428d6a3ec27704229552e3ad636a15c0c0
Alex <threebowpr@gmail.com>
2020-11-22 20:39:50 -0800

    Clean up checkbox, support external value prop changes, add switch option

--- vue/general/components/CheckboxInput/CheckboxInput.vue
+++ vue/general/components/CheckboxInput/CheckboxInput.vue
@@ -4,8 +4,9 @@
 
 		.control
 			.is-flex.is-flex-direction-row.is-align-items-center
-				.custom-checkbox.mr-2.is-flex-shrink-0(:class="{'is-circular': circular}", :disabled="disabled", @click="toggle")
-					icon(:class="{'is-active': selectedValue}", icon="fas fa-check")
+				.mr-2.is-flex-shrink-0(:class="[checkboxClass, activeClass]", :disabled="disabled", @click="toggle")
+					.switch-inner(v-if="this.switch", :class="activeClass")
+					icon(v-else, :class="activeClass", icon="fas fa-check")
 
 				input-label(:label="label")
 
@@ -30,11 +31,23 @@
 				type: Boolean,
 				default: false
 			},
+			switch: {
+				type: Boolean,
+				default: false
+			},
 			disabled: {
 				type: Boolean,
 				default: false
 			},
 			help: String
+		},
+		watch: {
+			value(val) {
+				//prop changed extenrally
+				if(val !== this.selectedValue) {
+					this.selectedValue = val;
+				}
+			}
 		},
 		methods: {
 			toggle() {
@@ -47,6 +60,18 @@
 		computed: {
 			id() {
 				return "checkbox" + this._uid;
+			},
+			activeClass() {
+				return {
+					"is-active": this.selectedValue
+				};
+			},
+			checkboxClass() {
+				return {
+					"switch-input": this.switch,
+					"checkbox-input": !this.switch,
+					"is-circular": !this.switch && this.circular
+				};
 			}
 		}
 	};
