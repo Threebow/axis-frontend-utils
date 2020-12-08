9a00cffbe84a2469ad97cfde1a23045b6cb06073
Alex <threebowpr@gmail.com>
2020-12-08 01:28:04 -0800

    Add panel components

---
+++ vue/general/components/Panel/Panel.vue
@@ -0,0 +1,13 @@
+<template lang="pug">
+	.panel
+		p.panel-heading(v-if="title") {{ title }}
+		slot
+</template>
+
+<script>
+	export default {
+		props: {
+			title: String
+		}
+	};
+</script>
---
+++ vue/general/components/Panel/PanelBlock.vue
@@ -0,0 +1,15 @@
+<template lang="pug">
+	.panel-block
+		span.panel-icon(v-if="icon")
+			i(:class="icon")
+
+		slot
+</template>
+
+<script>
+	export default {
+		props: {
+			icon: String
+		}
+	};
+</script>
