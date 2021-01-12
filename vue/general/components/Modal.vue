f239430bfb18da1eaf1db9a220cdf0a8686e5ba3
Alex <threebowpr@gmail.com>
2021-01-11 23:44:18 -0800

    Add modal component

---
+++ vue/general/components/Modal.vue
@@ -0,0 +1,41 @@
+<template lang="pug">
+	.modal.is-active(v-if="isOpened")
+		.modal-background(@click="close")
+
+		.modal-content
+			slot
+
+		button.modal-close.is-large(@click="close")
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				isOpened: false
+			};
+		},
+		watch: {
+			isOpened(val) {
+				document.documentElement.classList.toggle("is-clipped", val);
+			}
+		},
+		methods: {
+			open() {
+				this.isOpened = true;
+			},
+			close() {
+				this.isOpened = false;
+			},
+			toggle() {
+				this.isOpened = !this.isOpened;
+			}
+		}
+	};
+</script>
+
+<style lang="scss" scoped>
+	.modal, .modal-close {
+		position: fixed !important;
+	}
+</style>
