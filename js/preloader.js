98fe1f2c613b1dd228303d3e19bbaf1f509e9f5c
Alex <threebowpr@gmail.com>
2021-01-12 17:42:40 -0800

    Rewrite preloader frontend code

--- js/preloader.js
+++ js/preloader.js
@@ -1,22 +1,31 @@
-const FADE_DURATION = 0.25;
+(() => {
+	const FADE_DURATION = 0.25;
 
-window.addEventListener("load", () => {
-	let duration = FADE_DURATION * 1000;
-	let end = Date.now() + duration;
-
+	//Ensure preloader element exists
 	let preloader = document.getElementById("preloader");
 	if(!preloader) return;
 
-	function fadeOut() {
-		let frac = (end - Date.now()) / duration;
+	//Clip html element
+	document.documentElement.classList.add("is-clipped");
 
-		if(frac > 0) {
-			preloader.style.opacity = frac;
-			requestAnimationFrame(fadeOut);
-		} else {
-			preloader.parentElement.removeChild(preloader);
+	//Start removal process on load
+	window.addEventListener("load", () => {
+		let duration = FADE_DURATION * 1000;
+		let end = Date.now() + duration;
+
+		document.documentElement.classList.remove("is-clipped");
+
+		function fadeOut() {
+			let frac = (end - Date.now()) / duration;
+
+			if(frac > 0) {
+				preloader.style.opacity = frac;
+				requestAnimationFrame(fadeOut);
+			} else {
+				preloader.parentElement.removeChild(preloader);
+			}
 		}
-	}
 
-	requestAnimationFrame(fadeOut);
-});
+		requestAnimationFrame(fadeOut);
+	});
+})();
